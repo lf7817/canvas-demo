2321021f7f642b4049c32ff07274a458fcc5eda0
lifan <535536456@qq.com>
2017-06-27 18:35:35 +0800

    demo2基本实现效果

--- demo2/index.js
+++ demo2/index.js
@@ -1,22 +1,77 @@
-var WINDOW_WIDTH = 1024
-var WINDOW_HEIGHT = 768
+var WINDOW_WIDTH = document.body.clientWidth
+var WINDOW_HEIGHT = document.body.clientHeight - 4
 var RADIUS = 8
-var MARGIN_TOP = 60
+var MARGIN_TOP = 120
 var MARGIN_LEFT = 30
-
+var endTime = new Date(2017, 5, 27, 18, 46, 36)
+var curShowTimeSeconds = 0
+var balls = []
+var colors = ['#33B5E5', '#0099CC', '#AA66CC', '#9933CC', '#99CC00', '#669900', '#FFBB33', '#FF8800', '#FF4444', '#CC0000']
 var canvasEl = document.getElementById('canvas')
 var ctx = canvasEl.getContext('2d')
 
 canvasEl.width = WINDOW_WIDTH
 canvasEl.height = WINDOW_HEIGHT
 
+curShowTimeSeconds = getCurrentShowTimeSeconds()
 render(ctx)
 
-function render (ctx) {
-  var hour = 12
-  var min = 34
-  var sec = 56
+setInterval(function () {
+  render(ctx)
+  update()
+}, 50)
 
+function update () {
+  var nextShowTimeSeconds = getCurrentShowTimeSeconds()
+  if (nextShowTimeSeconds !== curShowTimeSeconds) {
+    var nextHour = parseInt(nextShowTimeSeconds / 3600)
+    var nextMin = parseInt((nextShowTimeSeconds - nextHour * 3600) / 60)
+    var nextSec = parseInt(nextShowTimeSeconds % 60)
+
+    var curHour = parseInt(curShowTimeSeconds / 3600)
+    var curMin = (curShowTimeSeconds - curHour * 3600) / 60
+    var curSec = curShowTimeSeconds % 60
+
+    if (parseInt(nextHour / 10) !== parseInt(curHour / 10)) {
+      addBalls(MARGIN_LEFT, MARGIN_TOP, parseInt(nextHour / 10))
+    }
+    if (parseInt(nextHour % 10) !== parseInt(curHour % 10)) {
+      addBalls(MARGIN_LEFT + 15 * (RADIUS + 1), MARGIN_TOP, parseInt(nextHour % 10))
+    }
+
+    if (parseInt(nextMin / 10) !== parseInt(curMin / 10)) {
+      addBalls(MARGIN_LEFT + 39 * (RADIUS + 1), MARGIN_TOP, parseInt(nextMin / 10))
+    }
+    if (parseInt(nextMin % 10) !== parseInt(curMin % 10)) {
+      addBalls(MARGIN_LEFT + 54 * (RADIUS + 1), MARGIN_TOP, parseInt(nextMin % 10))
+    }
+
+    if (parseInt(nextSec / 10) !== parseInt(curSec / 10)) {
+      addBalls(MARGIN_LEFT + 78 * (RADIUS + 1), MARGIN_TOP, parseInt(nextHour / 10))
+    }
+    if (parseInt(nextSec % 10) !== parseInt(curSec % 10)) {
+      addBalls(MARGIN_LEFT + 93 * (RADIUS + 1), MARGIN_TOP, parseInt(nextSec % 10))
+    }
+
+    curShowTimeSeconds = nextShowTimeSeconds
+  }
+  updateBalls()
+  // console.log(balls.length)
+}
+
+function getCurrentShowTimeSeconds () {
+  var curTime = new Date()
+  var ret = endTime.getTime() - curTime
+  ret = Math.round(ret / 1000)
+  return ret >= 0 ? ret : 0
+}
+
+function render (ctx) {
+  var hour = parseInt(curShowTimeSeconds / 3600)
+  var min = (curShowTimeSeconds - hour * 3600) / 60
+  var sec = curShowTimeSeconds % 60
+
+  ctx.clearRect(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT)
   renderDigit(MARGIN_LEFT, MARGIN_TOP, parseInt(hour / 10), ctx)
   renderDigit(MARGIN_LEFT + 15 * (RADIUS + 1), MARGIN_TOP, parseInt(hour % 10), ctx)
   renderDigit(MARGIN_LEFT + 30 * (RADIUS + 1), MARGIN_TOP, 10, ctx)
@@ -25,6 +80,14 @@ function render (ctx) {
   renderDigit(MARGIN_LEFT + 69 * (RADIUS + 1), MARGIN_TOP, 10, ctx)
   renderDigit(MARGIN_LEFT + 78 * (RADIUS + 1), MARGIN_TOP, parseInt(sec / 10), ctx)
   renderDigit(MARGIN_LEFT + 93 * (RADIUS + 1), MARGIN_TOP, parseInt(sec % 10), ctx)
+
+  balls.forEach(function (ball) {
+    ctx.fillStyle = ball.color
+    ctx.beginPath()
+    ctx.arc(ball.x, ball.y, RADIUS, 0, 2 * Math.PI, false)
+    ctx.closePath()
+    ctx.fill()
+  })
 }
 
 function renderDigit (x, y, num, ctx) {
@@ -42,4 +105,43 @@ function renderDigit (x, y, num, ctx) {
       }
     }
   }
+}
+
+function addBalls (x, y, num) {
+  for (var i = 0; i < digit[num].length; i++) {
+    for (var j = 0; j < digit[num][i].length; j++) {
+      if (digit[num][i][j] === 1) {
+        var ball = {
+          x: x + j * 2 * (RADIUS + 1) + (RADIUS + 1),
+          y: y + i * 2 * (RADIUS + 1) + (RADIUS + 1),
+          g: 1.5 + Math.random(),
+          vx: Math.pow(-1, Math.ceil(1000 * Math.random())) * 4,
+          vy: -12,
+          color: colors[Math.floor(Math.random() * colors.length)]
+        }
+        balls.push(ball)
+      }
+    }
+  }
+}
+
+function updateBalls () {
+  balls.forEach(function (ball, index, arr) {
+    ball.x += ball.vx
+    ball.y += ball.vy
+    ball.vy += ball.g
+
+    if (ball.y >= WINDOW_HEIGHT - RADIUS) {
+      ball.y = WINDOW_HEIGHT - RADIUS
+      ball.vy = -ball.vy * 0.6
+      if (Math.abs(ball.vy) <= 2) {
+        // console.log(ball.vy)
+        ball.vy = 0
+        ball.y = WINDOW_HEIGHT - RADIUS
+      }
+    }
+    if (ball.x <= -RADIUS || ball.x >= WINDOW_WIDTH + RADIUS) {
+      arr.splice(index, 1)
+    }
+  })
 }
